3598b9fc9e0acdf18823f30fc3aaaf8e8e8338c2
Alex Vashchenkov <alex.vashchenkov@gmail.com>
2020-12-09 11:17:32 +0300

    Update

--- Осень/Октябрь/HW_04_10_2020/3.ml
+++
@@ -1,3 +0,0 @@
-let rec string_to_number s n = 
-	if n = String.length s - 1 then (int_of_char s.[0]) else (int_of_char s.[(String.length s) - 1 - n]) + 256 * (string_to_number s (n+1));;
-
--- Осень/Сентябрь/HW_20_09_2020/3.ml
+++
@@ -1,9 +0,0 @@
-let n = read_int();;
-
-let rec make_prob n = 
-	if n = 1. then (1. -. 1. /. 365.) else (make_prob (n-.1.)) *. (1. -. (n -. 1.) /. 365.);;
-
-print_float (1. -. (make_prob n));;
-
-
-
