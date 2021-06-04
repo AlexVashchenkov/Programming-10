e508d6936547072006a78e25af654b3f6d66f4b0
Alex Vashchenkov <alex.vashchenkov@gmail.com>
2021-06-04 12:40:52 +0300

    massive breakthrough

--- Winter/February/24_02_20/5.ml
+++ Winter/February/24_02_20/5.ml
@@ -1,9 +1,6 @@
-let s = "0123456789";;
-let s = "0000";;
+let s = ".7";;
 
-let s = "20.7";;
-
-let re = Str.regexp "^\\(\\([1-9][0-9]*\\|0\\).[0-9]*[1-9]\\) \\| \\(.[0-9]*[1-9]\\) | \\([1-9][0-9]*\\|0 .\\)$";;
+let re = Str.regexp "^\\(\\([1-9][0-9]+\\)? \\|\\(0?\\)\\).\\(\\([0-9]+[1-9]\\)?\\)$";;
 
 if Str.string_match re s 0 then 
 	Printf.printf "Matched: %s\n" (Str.matched_group 1 s)
